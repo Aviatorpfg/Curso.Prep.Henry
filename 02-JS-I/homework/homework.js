4fd7a9ae2535d4f7309c3c2ca7eeceac1cd0aaca
Aviatorpfg <produktionfluss@hotmail.com>
2021-12-24 17:20:47 -0500

    modifity3 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -2,22 +2,22 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = hola ;
-// Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = '1,3,15,30' ;
-const nuevaResta = 10 - 4 === 6
 
-// Crea una variable booleana: 
-Const, nuevoBool = null;
+// Crea una variable numérica, puede ser cualquier número:
+const nuevoNum = null;
+
+// Crea una variable booleana:
+const nuevoBool = null;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - 5 === 5;
-
+const nuevaResta = 10 - null === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40 ;
 
 // Resuelve el siguiente problema matemático:
-nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === null;
+
 
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
